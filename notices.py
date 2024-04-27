b3dbd8221a428d1325098bc7bd3f561cc7e80cb0
ghanteyyy <ghanteyyy@gmail.com>
2024-04-27 17:07:56 +0545

    Removed unnecessary functionalities for improved code modularity and reduced component dependencies

--- notices.py
+++ notices.py
@@ -1,7 +1,5 @@
 import os
-import sys
 import time
-import subprocess
 from urllib3.exceptions import InsecureRequestWarning
 import requests
 from bs4 import BeautifulSoup
@@ -11,6 +9,9 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 class Notices:
     def __init__(self):
+        self.session = requests.Session()
+        self.session.verify = False
+
         self.is_result_downloaded = False
         self.url = 'https://fohss.tu.edu.np/notices'
         self.downloads_path = os.path.join(os.environ['USERPROFILE'], 'Downloads')
@@ -28,73 +29,6 @@ class Notices:
 
         return pdf_name in os.listdir(self.downloads_path)
 
-    def download_pdf(self, event, download_link):
-        """
-        Download a PDF file.
-
-        Args:
-            - event: The event triggering the download.
-            - download_link (str): The URL to download the PDF file from.
-        """
-
-        pdf_name = os.path.basename(download_link)
-        pdf_path = os.path.join(self.downloads_path, pdf_name)
-
-        with open(pdf_path, 'wb') as f:
-            content = self.session.get(download_link, verify=False, stream=True)
-            contents = content.content
-            f.write(contents)
-
-    def reveal_in_explorer(self, event, pdf_name):
-        """
-        Open the file explorer and reveal the downloaded PDF.
-
-        Args:
-            - event: The event triggering the reveal action.
-            - pdf_name (str): The name of the downloaded PDF file.
-        """
-
-        FILE_BROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
-        pdf_location = os.path.join(self.downloads_path, pdf_name)
-
-        subprocess.run([FILE_BROWSER_PATH, '/select,', pdf_location])
-
-    def is_internet(self):
-        """
-        Check if there is an active internet connection.
-
-        Returns:
-        - bool: True if there is an active internet connection, False otherwise.
-        """
-
-        try:
-            requests.get('http://google.com')
-            return True
-
-        except requests.ConnectionError:
-            return False
-
-    def resource_path(self, file_name):
-        '''
-        Get absolute path to resource from temporary directory
-
-        In development:
-            Gets path of files that are used in this script like icons, images or
-            file of any extension from current directory
-
-        After compiling to .exe with pyinstaller and using --add-data flag:
-            Gets path of files that are used in this script like icons, images or
-            file of any extension from temporary directory
-        '''
-
-        try:
-            base_path = sys._MEIPASS  # PyInstaller creates a temporary directory and stores path of that directory in _MEIPASS
-
-        except AttributeError:
-            base_path = os.getcwd()
-
-        return os.path.join(base_path, 'assets', file_name)
-
     def get_notices(self):
         """
         Get notices related to BCA results.
@@ -104,8 +38,6 @@ class Notices:
         """
 
         notices = []
-        self.session = requests.Session()
-        self.session.verify = False
 
         request = self.session.get(self.url).content
         soup = BeautifulSoup(request, "html.parser")
